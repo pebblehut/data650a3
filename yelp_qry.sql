9b9a120d68b86115384020b60f127de9a805c4cc
David Stonehouse <d_stonehouse@yahoo.com>
2016-10-23 21:09:53 -0400

    yelp_qry: create view

--- yelp_qry.sql
+++ yelp_qry.sql
@@ -6,6 +6,76 @@ where yelp_user.user_id = yelp_rvw.user_id;
 
 select yelp_rvw.business_id
 , yelp_rvw.votes_cool
-, yelp_user.yelping_since
-from yelp_rvw, yelp_user
-where yelp_user.business_id = yelp_rvw.business_id;
+, yelp_rvw.votes_cool
+, yelp_biz.name
+, yelp_biz.name
+from yelp_rvw, yelp_biz, yelp_user
+where yelp_biz.business_id = yelp_rvw.business_id;
+
+
+select yelp_rvw.business_id
+, yelp_rvw.votes_cool
+, yelp_biz.name as Business_Name
+, yelp_user.name as User_Name
+from 
+yelp_rvw left join yelp_biz 
+on yelp_biz.business_id = yelp_rvw.business_id
+left join yelp_user 
+on yelp_user.user_id = yelp_rvw.user_id
+;
+
+select 
+yelp_biz.name as Business_Name
+, yelp_biz.city as Business_City
+, yelp_biz.state as Business_State
+, yelp_user.name as User_Name
+, yelp_user.fans as User_Fans
+, yelp_rvw.stars as Review_Stars
+, yelp_biz.stars as Business_Avg_Stars
+, yelp_user.average_stars as User_Avg_Stars
+from 
+yelp_rvw left join yelp_biz 
+on yelp_biz.business_id = yelp_rvw.business_id
+left join yelp_user 
+on yelp_user.user_id = yelp_rvw.user_id
+;
+
+create or replace view review_full 
+as (
+select 
+yelp_biz.name as Business_Name
+, yelp_biz.city as Business_City
+, yelp_biz.state as Business_State
+, yelp_biz.open as Business_Open
+, yelp_biz.review_count as Business_Review_Count
+, yelp_biz.attributes_delivery as Delivery
+, yelp_biz.attributes_Take_out as Take_out
+, yelp_biz.attributes_Price_range as Price_Range
+, yelp_biz.stars as Business_Avg_Stars
+, yelp_user.yelping_since as User_Yelping_Since
+, yelp_user.review_count as User_Review_Count
+, yelp_user.name as User_Name
+, yelp_user.fans as User_Fans
+, yelp_user.average_stars as User_Avg_Stars
+, yelp_user.votes_cool as User_Votes_Cool
+, yelp_user.votes_funny as User_Votes_Funny
+, yelp_user.votes_useful as User_Votes_Useful
+, yelp_rvw.stars as Review_Stars
+, yelp_rvw.votes_cool as Review_Votes_Cool
+, yelp_rvw.votes_useful as Review_Votes_Useful
+, yelp_rvw.votes_funny as Review_Votes_Funny
+from 
+yelp_rvw left join yelp_biz 
+on yelp_biz.business_id = yelp_rvw.business_id
+left join yelp_user 
+on yelp_user.user_id = yelp_rvw.user_id
+)
+;
+
+
+select Business_Name, 
+, User_Name
+, Business_Avg_Stars
+, User_Avg_Stars
+from review_full
+where Review_Stars > 2;
