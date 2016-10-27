1b6631101bb094cd07e8dae020f7c918b58408cc
David Stonehouse <d_stonehouse@yahoo.com>
2016-10-27 18:29:21 -0400

    yelp_user2.sql, screenshots

--- yelp_user2.sql
+++ yelp_user2.sql
@@ -15,19 +15,36 @@ yelping_since      date
 )
 row format delimited fields terminated by '\t';
 
-insert into yelp_user (yelping_since,review_count,fans,average_stars,name,user_id,votes_cool,votes_funny,votes_useful)
+insert into yelp_user (yelping_since,review_count,fans,
+average_stars,name,user_id,votes_cool,votes_funny,votes_useful)
 values('2009-05-01',60,0,3.08,'Jeffrey','PUFPaY9KxDAcGqfsorJp3Q',13,31,174);
-insert into yelp_user (yelping_since,review_count,fans,average_stars,name,user_id,votes_cool,votes_funny,votes_useful)
+insert into yelp_user (yelping_since,review_count,fans,
+average_stars,name,user_id,votes_cool,votes_funny,votes_useful)
 values('2013-05-01',46,2,3.84,'Robin','uK8tzraOp4M5u3uYrqIBXg',6,3,42);
-insert into yelp_user (yelping_since,review_count,fans,average_stars,name,user_id,votes_cool,votes_funny,votes_useful)
+insert into yelp_user (yelping_since,review_count,fans,
+average_stars,name,user_id,votes_cool,votes_funny,votes_useful)
 values('2011-11-01',52,1,3.17,'Kevin','I_47G-R2_egp7ME5u_ltew',11,9,54);
-insert into yelp_user (yelping_since,review_count,fans,average_stars,name,user_id,votes_cool,votes_funny,votes_useful)
+insert into yelp_user (yelping_since,review_count,fans,
+average_stars,name,user_id,votes_cool,votes_funny,votes_useful)
 values('2014-10-01',2,0,3,'Mike','PP_xoMSYlGr2pb67BbqBdA',0,0,0);
-insert into yelp_user (yelping_since,review_count,fans,average_stars,name,user_id,votes_cool,votes_funny,votes_useful)
+insert into yelp_user (yelping_since,review_count,fans,
+average_stars,name,user_id,votes_cool,votes_funny,votes_useful)
 values('2012-04-01',180,5,3.92,'Kavi','fhNxoMwwTipzjO8A9LFe8Q',85,76,223);
-insert into yelp_user (yelping_since,review_count,fans,average_stars,name,user_id,votes_cool,votes_funny,votes_useful)
+insert into yelp_user (yelping_since,review_count,fans,
+average_stars,name,user_id,votes_cool,votes_funny,votes_useful)
 values('2013-07-01',2,0,1,'Anonymous','KZuaJtFindQM9x2ZoMBxcQ',0,0,1);
-insert into yelp_user (yelping_since,review_count,fans,average_stars,name,user_id,votes_cool,votes_funny,votes_useful)
+insert into yelp_user (yelping_since,review_count,fans,
+average_stars,name,user_id,votes_cool,votes_funny,votes_useful)
 values('2010-08-01',29,1,4.39,'Emily','LWbYpcangjBMm4KPxZGOKg',6,9,71);
-insert into yelp_user (yelping_since,review_count,fans,average_stars,name,user_id,votes_cool,votes_funny,votes_useful)
+insert into yelp_user (yelping_since,review_count,fans,
+average_stars,name,user_id,votes_cool,votes_funny,votes_useful)
 values('2013-03-01',4,1,4,'Fred','m1FpV3EAeggaAdfPx0hBRQ',0,1,1);
+
+
+select user_id,name,yelping_since,review_count,fans,average_stars
+from yelp_user;
+
+select user_id,votes_cool,votes_funny,votes_useful 
+from yelp_user;
+
+
